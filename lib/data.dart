4d5ebb6ae6f3c2243e3320bdaa8478a565b0779d
6264384853 <tamrakardivyansh189@gmail.com>
2020-06-08 05:44:03 +0530

    Repository

---
+++ lib/data.dart
@@ -0,0 +1,17 @@
+List<String> images = [
+  "assets/christmas.jpg",
+  "assets/disney1.webp",
+  "assets/download.jpg",
+  "assets/indore.jpg",
+  "assets/jabalpur.jpg",
+  "assets/kchnarcity.jpg",
+];
+
+List<String> title= [
+  "Christmas Eve 2020",
+  "Disney World",
+  "Download",
+  "Inodre,City of Foodie",
+  "Jabalpur",
+  "Shiv Temple",
+];
